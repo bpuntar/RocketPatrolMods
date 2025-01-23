f871f5b15bbf79f414584ac82b3379646496fff7
Brandon Apuntar <bapuntar@ucsc.edu>
2025-01-22 20:33:02 -0800

    added new faster ship called stinky

---
+++ src/main.js
@@ -0,0 +1,21 @@
+//Name: Brandon Apuntar
+//Mod Title: Rocket Patrol Plus
+//Mods: New Spaceship (5pts)
+let config = {
+    type: Phaser.AUTO,
+    width: 640,
+    height: 480,
+    scene: [Menu, Play]
+}
+
+let game = new Phaser.Game(config)
+
+//reserve keyboard bindings
+let keyFIRE, keyRESET, keyLEFT, keyRIGHT
+
+//set UI sizes
+let borderUISize = game.config.height / 15
+let borderPadding = borderUISize / 3
+
+
+
