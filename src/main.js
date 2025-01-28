f38d8cede0917c5d36d52b0187efb099c081c2f2
Brandon Apuntar <bapuntar@ucsc.edu>
2025-01-27 16:02:26 -0800

    changes to main

--- src/main.js
+++ src/main.js
@@ -1,7 +1,15 @@
 // Name: Brandon Apuntar
 // Mod Title: Rocket Patrol Plus
+// Approximate time: 5 hours
 // Mods: New Spaceship (5pts), Particle Emitter (5pts), Enemy ship animation (3pts), New menu screen (3pts),
 // Countdown timer (3pts), Move while firing (1pt)
+
+// Citations:
+// samme countdown timer from:
+// https://phaser.discourse.group/t/countdown-timer/2471/9
+// Particle system inspiration from:
+// https://labs.phaser.io/edit.html?src=src\game%20objects\particle%20emitter\emit%20at%20pointer.js
+// adapted to use with .explode 
 let config = {
     type: Phaser.AUTO,
     width: 640,
